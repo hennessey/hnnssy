2469ca08accff0fe6e19eebcb9c2d1f19347b489
John Hennessey <jghennes@gmail.com>
2014-12-29 20:57:56 -0500

    Adding initial test stubs

---
+++ hnnssy.Tests/Features/BusTimes.feature
@@ -0,0 +1,10 @@
+﻿Feature: BusTimes
+	In order to avoid being late in the morning
+	As a user
+	I want to be told the bus times for the closest available bus stops
+
+@Bus_Times
+Scenario: The Bus Times page is loaded
+	Given The current time and date
+	When I load the bus times page
+	Then the closest bus time for the library stop is displayed
---
+++ hnnssy.Tests/Steps/BusTimesSteps.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace hnnssy.Tests.Steps
+{
+    [Binding]
+    public class BusTimesSteps
+    {
+        [Given(@"The current time and date")]
+        public void GivenTheCurrentTimeAndDate()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I load the bus times page")]
+        public void WhenILoadTheBusTimesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the closest bus time for the library stop is displayed")]
+        public void ThenTheClosestBusTimeForTheLibraryStopIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
