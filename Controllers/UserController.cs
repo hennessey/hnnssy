b772b4e42ca5bd442973d6332d424f9347e06a21
John Hennessey <jghennes@gmail.com>
2014-12-29 19:51:23 -0500

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace hnnssy.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.IsValid(user.UserName, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserName, user.RememberMe);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Login data is incorrect");
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Helpers;
+
+namespace hnnssy.Models
+{
+    public class User
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember on this computer")]
+        public bool RememberMe { get; set; }
+
+        public bool IsValid(string username, string password)
+        {
+            using (
+                var cn =
+                    new SqlConnection(
+                        @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\personal\hnnssy\hnnssy\App_Data\Users.mdf;Integrated Security=True")
+                )
+            {
+                string _sql = @"SELECT [Username] FROM [dbo].[System_Users] " +
+                              @"WHERE [Username] = @u AND [Password] = @p";
+
+                var cmd = new SqlCommand(_sql, cn);
+
+                cmd.Parameters
+                    .Add(new SqlParameter("@u", SqlDbType.NVarChar))
+                    .Value = username;
+                cmd.Parameters
+                    .Add(new SqlParameter("@p", SqlDbType.NVarChar))
+                    .Value = SHA1.Create(password);
+                cn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return true;
+                }
+                else
+                {
+                    reader.Dispose();
+                    cmd.Dispose();
+                    return false;
+                }
+
+            }
+        }
+    }
+}
